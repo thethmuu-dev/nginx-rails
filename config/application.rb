8ecdb46ebc00dd53e3543f79cdbd0296c5c8c111
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-10-12 09:01:04 +0630

    Blogs CRUD

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module BlogNginx
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.javascript_engine :js
+      g.helper            false
+      g.system_tests      false
+      g.test_framework    nil
+      g.jbuilder          false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
